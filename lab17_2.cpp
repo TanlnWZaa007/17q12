3181ec713566567e7e6549c70f8cb3efd998d760
TanlnWZaa007 <153242404+TanlnWZaa007@users.noreply.github.com>
2025-02-07 16:35:57 +0700

    Update lab17_2.cpp

--- lab17_2.cpp
+++ lab17_2.cpp
@@ -1,19 +1,7 @@
-#include<iostream>
-using namespace std;
-
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
-}
-
-int main(){
-	char *p;
-	int N;	
-	cout << "N = ";
-	cin >> N;		
-	myString(p,N);
-	cout << p;	
-	delete [] p;
-	return 0;
+void myString(char *&p, int N) {
+    p = new char[N + 1]; // จองหน่วยความจำเพิ่ม 1 เพื่อเก็บเครื่องหมาย NULL ('\0') ที่ท้ายสตริง
+    for (int i = 0; i < N; i++) {
+        p[i] = 'A' + i; // กรอกตัวอักษรจาก A ไปเรื่อยๆ
+    }
+    p[N] = '\0'; // เพิ่มเครื่องหมาย NULL ที่ท้ายสตริง
 }
